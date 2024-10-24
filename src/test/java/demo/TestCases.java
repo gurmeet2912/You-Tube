f88d3ea9a59ea4fcf52f9faa868f31854d25ae70
unknown <gill.gurmeet29@gmail.com>
2024-10-24 17:39:14 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,15 +17,24 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import java.time.Duration;
+import java.util.List;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
-import demo.wrappers.Wrappers;
+//import demo.wrappers.Wrappers;
 
 public class TestCases extends ExcelDataProvider{ // Lets us read the data
         ChromeDriver driver;
+        
 
         /*
          * TODO: Write your tests here with testng @Test annotation.
@@ -57,11 +67,135 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
 
                 driver.manage().window().maximize();
         }
+         @Test
+         public void testCase01() throws InterruptedException{
+                System.out.println("Start Test Case : Test Case 01");
+                driver.get("https://www.youtube.com/");
+                String url=driver.getCurrentUrl();
 
-        @AfterTest
+                Assert.assertTrue(url.contains("youtube"),"URL Verification Passed");               
+
+                //WebElement tripleLines = driver.findElement(By.cssSelector("#guide-icon"));
+                //tripleLines.click();
+
+                Thread.sleep(3000);
+
+                WebElement about = driver.findElement(By.xpath("//a[text()='About']"));
+                about.click();   
+                
+                List<WebElement> message= driver.findElements(By.xpath("//section[contains(@class, 'ytabout__content')]"));
+                 for(WebElement msg: message){
+                     System.out.println(msg.getText());
+                 }
+
+                Thread.sleep(3000);   
+                
+                System.out.println("End Test Case : Test Case 01");
+        }
+
+        @Test
+        public void testCase02() throws InterruptedException{
+                System.out.println("Start Test Case : Test Case 02");
+                driver.get("https://www.youtube.com/");
+
+               // WebElement tripleLines = driver.findElement(By.cssSelector("#guide-icon"));
+                //tripleLines.click();
+
+                Thread.sleep(3000);
+
+                WebElement films= driver.findElement(By.xpath("//div[@id='sections']//descendant::ytd-guide-section-renderer[3]/div/ytd-guide-entry-renderer[4]"));
+                films.click();
+
+                String pageTitle = driver.getTitle();
+                if(pageTitle.contains("Films")){
+                    System.out.println("Films Tab Clicked");
+                }
+
+                Thread.sleep(3000);
+
+                WebElement topSelling = driver.findElement(By.xpath("//span[text()='Top selling']"));
+                topSelling.click();
+
+                Thread.sleep(3000);
+
+            //     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            //   //  boolean moreMovies=true;
+            //   WebElement arrowButtonSelector = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#right-arrow-button > ytd-button-renderer > yt-button-shape > button")));
+
+            //    while(arrowButtonSelector.isDisplayed()){
+            //     arrowButtonSelector.click();
+            //     Thread.sleep(2000);
+            //    }                                
+                  
+               }       
+
+        @Test
+        public void testCase03() throws InterruptedException{
+                System.out.println("Start Test Case : Test Case 02");
+                driver.get("https://www.youtube.com/");
+        }
+
+        @Test
+        public void testCase04() throws InterruptedException{
+                System.out.println("Start Test Case : Test Case 02");
+                driver.get("https://www.youtube.com/");
+        }
+
+        @Test(dataProvider = "excelData", dataProviderClass = ExcelDataProvider.class)
+        public void testCase05(String searchTerms) throws InterruptedException{
+                System.out.println("Start Test Case : Test Case 05");
+                driver.get("https://www.youtube.com/");
+
+                System.out.println("Verify video views count: " + searchTerms);
+
+                WebElement searchBox = driver.findElement(By.xpath("//input[contains(@id, 'search')]"));
+                searchBox.sendKeys(searchTerms);
+                long totalCount = 10_00_00_000;
+                long totalViewsInCrores = 0;
+
+
+                List<WebElement> views = driver.findElements(By.xpath("//*[@id='metadata-line']/span[1]"));
+               
+                for(WebElement v : views){
+                Pattern pattern = Pattern.compile("(\\d+)([kKmM]?) views");
+                Matcher matcher = pattern.matcher((CharSequence) v);
+
+            if (matcher.find()) {
+                int number = Integer.parseInt(matcher.group(1));
+                String suffix = matcher.group(2).toLowerCase();
+
+                if (suffix.equals("k")) {
+                    number *= 1000;
+                } else if (suffix.equals("m")) {
+                    number *= 1000000;
+                }
+
+                totalViewsInCrores += number;
+                if(totalViewsInCrores==totalCount){
+                    System.out.println("End Test Case");
+                }
+            }
+        }
+              
+                // for(WebElement v : views){
+                //     String s= v.getText();
+                //     String[] parts = s.split("\\D+");
+                //     if (parts.length > 0) {
+                //         String numberStr = parts[0];
+                //         int number = Integer.parseInt(numberStr);
+
+                // }
+
+
+
+        }
+      
+
+               
+        @AfterTest 
         public void endTest() {
-                driver.close();
+           driver.close();
                 driver.quit();
-
         }
 }
